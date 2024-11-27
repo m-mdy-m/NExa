3de7f96ea4d6d1b41161bc1261e1c26970ba77a7
mahdi <bitsgenix@gmail.com>
2024-11-27 14:27:52 +0330

    refactor(modules): update simplify `fetch_articles` function, remove `construct_search_url` import, and update menu options.

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,7 +2,7 @@ import requests
 from urllib.parse import parse_qs, urlsplit
 from termcolor import colored
 from devto.parser import DevToHTMLParser
-from utils import format_time,display_articles
+from utils import format_time, display_articles
 from menu import get_user_choice
 
 def fetch_articles():
@@ -14,24 +14,7 @@ def fetch_articles():
     url = base_url + path
     print(colored(f"\nFetching articles from: {url}\n", "cyan", attrs=["bold"]))
     try:
-        response=''
-        search_query = path.split("?")[-1]
-        print('search_query:', search_query)
-        print('base_url',base_url)
-        print('url',url)
-        query_params = {}
-        if search_query.startswith("q=") or search_query.startswith("utf8"):
-            for param in search_query.split("&"):
-                key, value = param.split("=")
-                query_params[key] = value
-            query_params.setdefault("sort_by", "published_at")
-            query_params.setdefault("sort_direction", "desc")
-            
-            print('Query Parameters:', query_params)
-            # Fetch articles with properly constructed query parameters
-            response = requests.get(f"{base_url}/search", timeout=10, params=query_params)
-        else:
-            response = requests.get(url,timeout=10)
+        response = requests.get(url,timeout=10)
         if response.status_code == 200:
             print(colored(">>> Fetch successful! Parsing articles...\n", "green", attrs=["bold"]))
             parser = DevToHTMLParser()
@@ -45,6 +28,3 @@ def fetch_articles():
             print(colored(f"Failed to fetch articles. Status code: {response.status_code}", "red"))
     except requests.exceptions.RequestException as e:
         print(colored(f"An error occurred: {e}", "red"))
-
-if __name__ == "__main__":
-    fetch_articles()
